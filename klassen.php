02165a90450d0531fa0fa36be0535729904e56a4
PhilIsHere <48513743+PhilIsHere@users.noreply.github.com>
2022-06-13 14:03:01 +0200

    Abstrakte Klassen und Abstrakte Methoden Interfaces

--- klassen.php
+++ klassen.php
@@ -5,9 +5,9 @@ class Person implements adressen {
 //    private string $firstName;
 //    private string $lastname;
 
-    //Ein Konstruktor wird so eröffntet, dass man zunächst die Zugriffsrechte definiert (std: Protected) und __construct(){} schreibt
+    //Ein Konstruktor wird so eröffnet, dass man zunächst die Zugriffsrechte definiert (std: Protected) und __construct(){} schreibt
     //Die zwei Unterstriche sind sog. Magic Methods, die in allen Klassen bereits vordefiniert sind.
-    public function __construct(protected string $firstName = '', protected string $lastname = '')
+    public function __construct(protected string $firstName = '', protected string $lastname = '') //Im Konstruktor kann man einige Variablen mit Zugriffsrechten angeben. Nicht zuviele
     {
         //Methodenaufruf über den -> Operator
 //        $this->firstName = $firstName;
@@ -17,47 +17,63 @@ class Person implements adressen {
     public function getFirstName() : string {
         return $this->firstName;
     }
-
-    public function getLastName() : string{
-        return $this->lastname;
-    }
-
     public function setFirstName(string $firstName) : self {
         $this->firstName = $firstName;
         return $this;
     }
 
+    public function getLastName() : string{
+        return $this->lastname;
+    }
     public function setLastName(string $lastName) : self {
         $this->lastname = $lastName;
         return $this;
     }
 
-    public function generateAdresse($strassenname, $hausnummer) {
-        $this->$strassenname = $strassenname;
-        $this->$hausnummer = $hausnummer;
-        $adresse = $this->$strassenname .' '. $this->$hausnummer;
-        return $adresse;
+    public function setAdresse(string $adresse){
+        $this->adresse = $adresse;
+        return $this;
     }
+    //In der Praxis teilt man normalerweise Informationen möglichst atomar/kleinteilig. Heißt: Name getrennt vom Vornamen, PLZ getrennt von Straßennamen usw.
+    public function getAdresse():string{
+        return $this->adresse;
+    }
+
+    public function setPostleitzahl(string $postleitzahl){
+        $this->postleitzahl = $postleitzahl;
+        return $this;
+    }
+
+    public function getPostleitzahl():string{
+        return $this->postleitzahl;
+    }
+    //Eine abstrakte Methode ist eine nicht implementierte Methode, sie muss aber in der vererbenden Klasse vorkommen und implementiert werden.
+    abstract public function generateKundennummer():string;
+
+//    public function generateAdresse() {
+//        $this->strassenname = $strassenname; // Nicht best-practice. Besser: Ein Getter und Setter pro Variable, wenn man die Adresse im Objekt speichern möchte.
+//        $this->hausnummer = $hausnummer;
+//        return $this->strassenname .' '. $this->hausnummer;
+//    }
+
 }
 
 class Kunde extends Person{
-
-    //ToDo Philip: Klasse "kunde" Erstellen die "Person" extended. Eigenschaften: Kundennummer-->Base64 Hash aus Vor und Nachnamen
+    //ToDo Philip: Klasse "Kunde" Erstellen die "Person" extended. Eigenschaften: Kundennummer-->Base64 Hash aus Vor und Nachnamen
     public function generateKundennummer() : string {
-        $name = $this->firstName . ' ' . $this->getLastName();
-        $kundennummer = base64_encode($name);
-        return (string)$kundennummer;
+        return base64_encode($this->getFirstName() . ' ' . $this->getLastName()); //Verknüpfung reicht über return. Erst ab verarbeitung brauche ich Variablen.
     }
 
     public function __toString() : string
     {
-        return 'Vor- und Nachname: ' . $this->getFirstName() . ' ' . $this->getLastName().PHP_EOL . 'Kundennummer: '.$this->generateKundennummer().PHP_EOL.'Adresse: '.$this->generateAdresse('Sonnenweg', 1);
+        return 'Vor- und Nachname: ' . $this->getFirstName() . ' ' . $this->getLastName().PHP_EOL . 'Kundennummer: '.$this->generateKundennummer().PHP_EOL.'Adresse: '.$this->getAdresse() . ' '.$this->getPostleitzahl();
     }
+
 }
 
 
 $erstePerson = new Kunde();
-$erstePerson->setFirstName('Philip')->setLastName('Nachname');
+$erstePerson->setFirstName('Philip')->setLastName('Nachname')->setAdresse('Sonnenweg 1')->setPostleitzahl('12345');
 
 
 //Nutzt automatisch die toString Methode, da wir einen typecast auf "string" angeben/erwarten.
